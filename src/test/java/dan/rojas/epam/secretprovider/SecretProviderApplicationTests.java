e40a605cc20c46ab2490925ff61fa7dc12ce6969
Daniel Rojas <v-darojas@expediagroup.com>
2022-07-06 17:29:22 -0500

    first commit

---
+++ src/test/java/dan/rojas/epam/secretprovider/SecretProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.rojas.epam.secretprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
